c02da36875a5c2ea5dda4055bea5ff6d58cdc2cb
vood <igor-vood@gmail.com>
2025-07-01 14:50:45 +0300

    i

---
+++ src/main/kotlin/ru/vood/context/receivers/example/contextreceiversexample/ContextReceiversExampleApplication.kt
@@ -0,0 +1,11 @@
+package ru.vood.context.receivers.example.contextreceiversexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContextReceiversExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ContextReceiversExampleApplication>(*args)
+}
---
+++ src/test/kotlin/ru/vood/context/receivers/example/contextreceiversexample/ContextReceiversExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vood.context.receivers.example.contextreceiversexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContextReceiversExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
